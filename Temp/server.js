ef9a6905fa0d7737ed881cddbaefdfd4157ac18f
Dia Jubara <djubara@hotmail.com>
2024-05-19 18:39:24 -0500

    updated routs.js and server.js

--- Temp/server.js
+++ Temp/server.js
@@ -1,22 +1,21 @@
 // Initialize express server
 const express = require('express');
 
+const routes = require('./lib/routes/routes.js');
+
 // Assigning the express server to the app variable so that we can use it in other files instead of using express() function again.
 const app = express();
 
+// Setting up the middleware for api routes
+app.use(routes);
+
+// Setting up the middleware for Public folder
+app.use(express.static('./lib/public', { extensions: ['html'] }));
 
 // Assigning the port number to the PORT variable so that we can use it in other files instead of using the port number again.
 const PORT = process.env.PORT || 3001;
 
-// Setting up the middleware for Public folder
-app.use(express.static('public', { extensions: ['html'] }));
 
-// app.get('/api/notes', (req, res) => { });
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-
-// });
 
 
 
